c8e71521ec48af3380104ae6649012fb402abbe3
genrf <rmacias@autentia.com>
2013-04-18 16:35:34 +0200

    statemanager2

---
+++ scripts/app/controllers/LoginController.js
@@ -0,0 +1,19 @@
+define(["ember"], function(Ember){
+	var loginController = Ember.Object.create({
+		userName: '',
+		repeatedUserName: '',
+		hasError: false,
+		errorMsg: '',
+		attemptLogin: function(view){
+			var userName = this.get('userName');
+				repeatedUserName = this.get('repeatedUserName');
+			console.log('attemptLogin');
+			//Añadir comprobacion de que no esten vacios
+			if(userName == repeatedUserName){
+				console.log('Nombres coinciden');
+				App.stateManager.send('loginSuccess');
+			}
+		}
+	 });
+	return loginController;
+}); 
