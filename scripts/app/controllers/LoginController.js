3895f8c402c27fe53aab78bf13daffe409d95558
genrf <rmacias@autentia.com>
2013-04-19 10:05:23 +0200

    añadida condicion de logeo, y fichero de trucos(a ir añadiendo contenido)

--- scripts/app/controllers/LoginController.js
+++ scripts/app/controllers/LoginController.js
@@ -3,16 +3,31 @@ define(["ember"], function(Ember){
 		userName: '',
 		repeatedUserName: '',
 		hasError: false,
-		errorMsg: '',
+		errorMismatch: false,
+		errorEmpty: false,
 		attemptLogin: function(view){
+			//Reinicializamos las variables de error
+			this.set('hasError', false);
+			this.set('errorMismatch', false);
+			this.set('errorEmpty',false);
 			var userName = this.get('userName');
 				repeatedUserName = this.get('repeatedUserName');
 			console.log('attemptLogin');
 			//Añadir comprobacion de que no esten vacios
-			if(userName == repeatedUserName){
-				console.log('Nombres coinciden');
-				App.stateManager.loginSuccess();
+			if(userName != "" || repeatedUserName != ""){
+				if(userName == repeatedUserName){
+					console.log('Nombres coinciden');
+					App.stateManager.loginSuccess();
+				}
+				else {
+					this.set('hasError', true);
+					this.set('errorMismatch', true);
+				}	
+			}else{
+				this.set('hasError', true);
+				this.set('errorEmpty',true);
 			}
+			
 		}
 	 });
 	return loginController;
--- scripts/loc/en.js
+++ scripts/loc/en.js
@@ -1,11 +1,12 @@
 define(function(){
 	loc = {
 		'main.application.title': 'First App with Ember.js',
-		'index.login.fail': 'The names do not match',
+		'index.login.fail.mismatch': 'The names do not match',
+		'index.login.fail.empty': 'Required fields',
 		'index.login.loginButton': 'Sign in',
 		'index.login.nameLabel': 'Type your name: ',
 		'index.login.nameRepeatLabel': 'Re-type your name: ',
-		'menu.login.noLog': 'You have to enter an username.',
+		'menu.login.noLog': 'You have to enter an username',
 		'menu.login.backToIndex': 'Back',
 		'menu.loged.welcomeMsg': 'Welcome',
 		'menu.button.colorListButton': 'Show color list',
